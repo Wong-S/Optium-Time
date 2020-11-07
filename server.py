b7cc556c9a7a93e4a10889fb8c86359424f0713c
Sarah Wong <swong3310@gmail.com>
2020-11-07 05:11:17 +0000

    Add new journal routes to pass user_id to backend database

--- server.py
+++ server.py
@@ -39,7 +39,7 @@ def create_homepage():
 def display_users_email():
     """Show all users email addresses and link to user's profile"""
 
-    users_emails = crud.get_user_email()
+    users_emails = crud.get_user()
 
     return render_template("user_details.html", users_emails=users_emails)
 
@@ -69,6 +69,7 @@ def register_user():
         flash(
             "New account created successfully! Please log in"
         )  # FIXME: Change Flash messages!!!
+
     else:
         flash("Email is associated with an account. Try again!")
 
@@ -84,9 +85,13 @@ def check_login_credentials():
 
     match_passwords = crud.check_password(email, password)
 
+    user_details = crud.get_user_by_email(
+        email
+    )  # NOTE: Return the specified OBJECT relating to that exact email. From there, can call instance attributes
+
     if match_passwords == True:
         flash("Logged in!")
-        return redirect("/user-page")
+        return render_template("user_page.html", user=user_details)
     else:
         flash("Email or password do not match. Try again!")
         return redirect("/")
@@ -96,50 +101,57 @@ def check_login_credentials():
 # User Page Route: What displays here is Journals, Sleep Log, Playlists Selection, etc:
 
 
-@app.route("/user-page")
-def display_user_options():
-    """Return user's next selection"""
+# @app.route("/user-page")
+# def display_user_options():
+#     """Return user's next selection"""
 
-    return render_template("user_page.html")
+#     return render_template("user_page.html")
 
 
 # =====================================================================
 # NOTE: Journal Entry Routes Start Here:
-@app.route("/journal")
-def journal_entry():
+@app.route("/journal/<user_id>")
+def journal_entry(user_id):
     """Return journal page """
 
-    return render_template("journal.html")
+    return render_template(
+        "journal.html", user_id=user_id
+    )  # NOTE:Passing in the user's ID here!
 
 
 # NOTE: CHECKING if Journal Entries actually got seeded and display correctly
-@app.route("/journal-current-entries")
-def display_journal_information():
+@app.route("/journal-current-entries/<user_id>")
+def display_journal_information(user_id):
     """Show all user's journal information"""
 
-    journal_list = crud.get_user_journal()
+    user_obj = crud.check_user_to_journal_id(user_id)
 
-    return render_template("journal_details.html", journal_list=journal_list)
+    return render_template("journal_details.html", user_obj=user_obj)
 
 
-@app.route("/journal-new-entries")
-def display_new_journal():
+@app.route("/journal-new-entries/<user_id>")
+def display_new_journal(user_id):
     """Return new journal page"""
 
-    return render_template("journal_creation.html")
+    return render_template("journal_creation.html", user_id=user_id)
 
 
-@app.route("/journals", methods=["POST"])
-def register_journal_entry():
+# NOTE: Need to ensure ID match to make a journal entry!
+@app.route("/journals/<user_id>", methods=["POST"])
+def register_journal_entry(user_id):
     """Create a new journal entry per user"""
 
     entry_name = request.form.get("entry-name")
     entry_details = request.form.get("entry-details")
+    user_id = int(
+        user_id
+    )  # NOTE FIXME: CODE REVIEW ?? Does a Foreign Key really need to be specified here? And converted from user_id string to int?
 
-    crud.create_journal_entry(entry_name, entry_details)
+    # if user_id matches True:
+    crud.create_journal_entry(user_id, entry_name, entry_details)
     flash("New entry made!!")
 
-    return redirect("/journals")
+    return render_template("journal.html", user_id=user_id)
 
 
 if __name__ == "__main__":
